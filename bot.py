34d44f45020444f3843e99e45c98916a7a03f25e
Girayzeynal <girayzeynal@gmail.com>
2025-11-09 11:40:51 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+import telebot
+
+# Token ortam değişkeni (Fly.io için)
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_cmd(message):
+    bot.send_message(message.chat.id, "Bot ✅ Çalışıyor!")
+
+bot.infinity_polling()
