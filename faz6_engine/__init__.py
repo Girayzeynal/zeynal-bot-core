6934d476d973c9b8ad10aee4ffe9508f562ed4db
Girayzeynal <girayzeynal@gmail.com>
2025-11-16 19:59:39 +0300

    Update __init__.py

--- faz6_engine/__init__.py
+++ faz6_engine/__init__.py
@@ -1,8 +1,10 @@
-"""
-FAZ-6 paket tanımlayıcı.
-Buradan sadece ana motoru dışarı açıyoruz.
-"""
+# faz6_engine/__init__.py
+# Paket dışına açılan arayüz
 
 from .faz6_engine_main import run_faz6_engine
+from .faz6_coupon import build_coupon_message
 
-__all__ = ["run_faz6_engine"] 
+__all__ = [
+    "run_faz6_engine",
+    "build_coupon_message",
+] 
