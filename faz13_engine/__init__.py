106d6d371516ff623043dbafcb17c922ac96c8f0
Girayzeynal <girayzeynal@gmail.com>
2025-11-23 23:08:38 +0300

    Create __init__.py

---
+++ faz13_engine/__init__.py
@@ -0,0 +1,13 @@
+from .faz13_fusion import (
+    FusionInput,
+    normalize_manual_text,
+    normalize_api_data,
+    normalize_visual_meta,
+)
+
+__all__ = [
+    "FusionInput",
+    "normalize_manual_text",
+    "normalize_api_data",
+    "normalize_visual_meta",
+]
