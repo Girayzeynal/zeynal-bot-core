22b67d06e9184468b6ef15e20e499ab951b6419b
Girayzeynal <girayzeynal@gmail.com>
2025-11-28 13:54:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -582,8 +582,9 @@ from faz13_engine.faz13_orchestrator import (
     faz13_league_coupon,
     faz13_live_coupon,
 )
+
 from faz13_engine.faz13_god_layer import (
-    faz13_god_pipeline,
+    run_faz13_with_god_layer,
     faz13_god_text,
 )
 
