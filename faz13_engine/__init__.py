586aa3a71efc8d5b77b0435c0f10b555045711a3
Girayzeynal <girayzeynal@gmail.com>
2025-12-06 20:03:03 +0300

    Update __init__.py

--- faz13_engine/__init__.py
+++ faz13_engine/__init__.py
@@ -1,5 +1,34 @@
-# faz13_engine package initializer
-# Bu dosya bilerek boş tutulur. Export veya import eklenmez.
-# FAZ-13 modülleri main.py üzerinden _safe_import ile dinamik yüklenir.
+"""
+FAZ-13 core package (light mode).
 
-# Kesinlikle BOM, Türkçe karakter veya gizli unicode olmayacak şekilde sade bırakıldı.
+Bu profilde Ultra OCR v3 TORCH/EASYOCR kullanmadan,
+tamamen kapalı tutuluyor. Sadece hafif Python modülleri
+expose ediliyor.
+"""
+
+from .faz13_orchestrator import (
+    normalize_manual_text,
+    normalize_visual_meta,
+    normalize_api_data,
+    run_faz13_auto_pipeline,
+    faz13_daily_coupon,
+    faz13_upcoming_coupon,
+    faz13_league_coupon,
+    faz13_live_coupon,
+)
+
+from .faz13_god_layer import run_faz13_with_god_layer
+from .league_autodetect import guess_league
+
+__all__ = [
+    "normalize_manual_text",
+    "normalize_visual_meta",
+    "normalize_api_data",
+    "run_faz13_auto_pipeline",
+    "faz13_daily_coupon",
+    "faz13_upcoming_coupon",
+    "faz13_league_coupon",
+    "faz13_live_coupon",
+    "run_faz13_with_god_layer",
+    "guess_league",
+]
