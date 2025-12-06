38aef6eda4ccbae8c974905b8396df49959b896d
Girayzeynal <girayzeynal@gmail.com>
2025-12-06 12:02:01 +0300

    Update __init__.py

--- faz13_engine/__init__.py
+++ faz13_engine/__init__.py
@@ -1,27 +1,5 @@
-from .faz13_orchestrator import (
-    normalize_visual_meta,
-    normalize_api_data,
-    run_faz13_auto_pipeline,
-    faz13_daily_coupon,
-    faz13_upcoming_coupon,
-    faz13_league_coupon,
-    faz13_live_coupon,
-)
+# faz13_engine package initializer
+# Bu dosya bilerek boş tutulur. Export veya import eklenmez.
+# FAZ-13 modülleri main.py üzerinden _safe_import ile dinamik yüklenir.
 
-from .faz13_god_layer import run_faz13_with_god_layer
-
-__all__ = [
-    # Normalizerlar
-    "normalize_visual_meta",
-    "normalize_api_data",
-    "run_faz13_auto_pipeline",
-
-    # Kupon motorları
-    "faz13_daily_coupon",
-    "faz13_upcoming_coupon",
-    "faz13_league_coupon",
-    "faz13_live_coupon",
-
-    # GOD-LAYER
-    "run_faz13_with_god_layer",
-] 
+# Kesinlikle BOM, Türkçe karakter veya gizli unicode olmayacak şekilde sade bırakıldı.
