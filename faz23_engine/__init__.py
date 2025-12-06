79c1957f79f70cc5032ff25c4f0a05ea36552290
Girayzeynal <girayzeynal@gmail.com>
2025-12-06 14:04:06 +0300

    Create __init__.py

---
+++ faz23_engine/__init__.py
@@ -0,0 +1,11 @@
+from .faz23_meta_engine import (
+    faz23_prematch_predict,
+    faz23_live_predict,
+    faz23_news_enrich,
+)
+
+__all__ = [
+    "faz23_prematch_predict",
+    "faz23_live_predict",
+    "faz23_news_enrich",
+]
