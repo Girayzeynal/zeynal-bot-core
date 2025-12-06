d223b6ce225ba5ca82bf051aad399c46bb3f498a
Girayzeynal <girayzeynal@gmail.com>
2025-12-06 23:10:24 +0300

    Update __init__.py

--- faz13_engine/__init__.py
+++ faz13_engine/__init__.py
@@ -1,18 +1,6 @@
-# faz13_engine/__init__.py
-"""
-FAZ-13 engine package
-
-Bu paket import edilirken ASLA crash olmamalı.
-- main.py içinde _safe_import zaten modülleri tek tek güvenli şekilde çekiyor.
-- Burada sadece gerçekten var olan fonksiyonları dışarı açıyoruz.
-"""
-
-from .faz13_orchestrator import run_faz13_auto_pipeline
-from .faz13_god_layer import run_faz13_with_god_layer
-from .league_autodetect import guess_league
-
-__all__ = [
-    "run_faz13_auto_pipeline",
-    "run_faz13_with_god_layer",
-    "guess_league",
-] 
+from .faz23_max import (
+    Faz23MaxConfig,
+    faz23_max_predict,
+    faz23_max_comment,
+    build_fusion_vector,
+)
