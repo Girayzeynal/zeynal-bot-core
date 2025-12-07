8448a9791152613c1ead2ab9c7e06df4b83c0c0c
Girayzeynal <girayzeynal@gmail.com>
2025-12-07 05:26:24 +0300

    Update __init__.py

--- faz23_engine/__init__.py
+++ faz23_engine/__init__.py
@@ -1,6 +1,13 @@
-from .faz23_max import (
-    Faz23MaxConfig,
-    faz23_max_predict,
-    faz23_max_comment,
-    build_fusion_vector,
-) 
+from .faz13_god_layer import run_faz13_with_god_layer
+from .faz13_orchestrator import (
+    normalize_manual_text,
+    normalize_visual_meta,
+    normalize_api_data,
+    run_faz13_auto_pipeline,
+    faz13_daily_coupon,
+    faz13_upcoming_coupon,
+    faz13_league_coupon,
+    faz13_live_coupon,
+)
+from .league_autodetect import guess_league
+from .ultra_ocr_v3 import ultra_ocr_engine_v3
