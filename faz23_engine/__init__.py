f12a021e0ce128479a73e991a2c499327ba20457
Girayzeynal <girayzeynal@gmail.com>
2025-12-09 18:38:10 +0300

    Update __init__.py

--- faz23_engine/__init__.py
+++ faz23_engine/__init__.py
@@ -1,58 +1,11 @@
-# FAZ-23 ENGINE __init__.py
-
-# --- FAZ-13 GOD LAYER ---
-from faz13_engine.faz13_god_layer import run_faz13_with_god_layer
-
-# --- FAZ-13 ORCHESTRATOR ---
-from faz13_engine.faz13_orchestrator import (
-    normalize_manual_text,
-    normalize_visual_meta,
-    normalize_api_data,
-    run_faz13_auto_pipeline,
-    faz13_daily_coupon,
-    faz13_upcoming_coupon,
-    faz13_league_coupon,
-    faz13_live_coupon,
+from .faz23_meta_engine import (
+    faz23_prematch_predict,
+    faz23_live_predict,
+    faz23_news_enrich,
 )
 
-# --- LEAGUE AUTO-DETECT ---
-from faz13_engine.league_autodetect import guess_league
-
-# --- ULTRA OCR ENGINE v3 ---
-from faz13_engine.ultra_ocr_v3 import ultra_ocr_engine_v3
-
-# --- FAZ-23 MAX ENGINE (opsiyonel) ---
-try:
-    from .faz23_max import (
-        Faz23MaxConfig,
-        faz23_max_predict,
-        faz23_max_comment,
-        build_fusion_vector,
-    )
-except ImportError:
-    Faz23MaxConfig = None
-    faz23_max_predict = None
-    faz23_max_comment = None
-    build_fusion_vector = None
-
 __all__ = [
-    # FAZ-13 core
-    "normalize_manual_text",
-    "normalize_visual_meta",
-    "normalize_api_data",
-    "run_faz13_auto_pipeline",
-    "faz13_daily_coupon",
-    "faz13_upcoming_coupon",
-    "faz13_league_coupon",
-    "faz13_live_coupon",
-    # GOD LAYER
-    "run_faz13_with_god_layer",
-    # OCR + league detect
-    "guess_league",
-    "ultra_ocr_engine_v3",
-    # FAZ-23 MAX (opsiyonel)
-    "Faz23MaxConfig",
-    "faz23_max_predict",
-    "faz23_max_comment",
-    "build_fusion_vector",
-] 
+    "faz23_prematch_predict",
+    "faz23_live_predict",
+    "faz23_news_enrich",
+]
