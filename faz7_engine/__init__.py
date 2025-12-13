c7fcc7cf0ba476be82695793d2e80ca72ca6e9d8
Girayzeynal <girayzeynal@gmail.com>
2025-12-13 17:38:32 +0300

    Update __init__.py

--- faz7_engine/__init__.py
+++ faz7_engine/__init__.py
@@ -1,3 +1,10 @@
-from .faz7_memory import Faz7MemoryEngine
+# -*- coding: utf-8 -*-
+"""
+FAZ-7 package exports (mimari uyum)
+main.py şunu yapabilsin:
+- from faz7_engine import faz7_memory
+"""
 
-__all__ = ["Faz7MemoryEngine"]
+from .faz7_memory import faz7_memory
+
+__all__ = ["faz7_memory"]
