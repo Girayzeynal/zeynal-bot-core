780aaa8b409726ec1ab4e2bf522018e265568c50
Girayzeynal <girayzeynal@gmail.com>
2025-12-13 17:30:48 +0300

    Update __init__.py

--- faz15_engine/__init__.py
+++ faz15_engine/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+FAZ-15 package exports (mimari uyum)
+main.py şunları yapabilsin:
+- from faz15_engine import faz15_preprocess, preprocess_image_bytes
+"""
 
 from .faz15_preprocess import faz15_preprocess, preprocess_image_bytes
 
