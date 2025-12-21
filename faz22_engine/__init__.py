f7148914e6edc472eb5a02e8f8809636167135cf
Girayzeynal <girayzeynal@gmail.com>
2025-12-21 23:38:45 +0300

    Create __init__.py

---
+++ faz22_engine/__init__.py
@@ -0,0 +1,4 @@
+from .faz22_meta import faz22_meta_engine
+from .faz22_state import apply_hint as faz22_apply_hint, get_weights as faz22_get_weights
+
+__all__ = ["faz22_meta_engine", "faz22_apply_hint", "faz22_get_weights"]
