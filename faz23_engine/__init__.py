3222e57ab03cc858a4a7188cd1118f14684dfe98
Girayzeynal <girayzeynal@gmail.com>
2025-12-21 23:59:15 +0300

    Update __init__.py

--- faz23_engine/__init__.py
+++ faz23_engine/__init__.py
@@ -1,3 +1,4 @@
 from .faz23_core import faz23_memory_write
+from .faz23_feedback import faz23_apply_result
 
-__all__ = ["faz23_memory_write"]
+__all__ = ["faz23_memory_write", "faz23_apply_result"]
