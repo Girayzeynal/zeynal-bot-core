5c456e9da8396f00492b5e725fad8ba09aa27f7f
Girayzeynal <girayzeynal@gmail.com>
2025-12-21 23:29:22 +0300

    Update __init__.py

--- faz17_engine/__init__.py
+++ faz17_engine/__init__.py
@@ -1,5 +1,4 @@
-# FAZ-17 package surface (TEK DOĞRU GİRİŞ)
-from .providers import odds_api_fetch_market as faz17_fetch_market  # provider
-from .faz17_market_fetcher import faz17_fetch_market_safe  # safety wrapper
+from .providers import faz17_fetch_market
+from .faz17_market_fetcher import faz17_fetch_market_safe
 
-__all__ = ["faz17_fetch_market", "faz17_fetch_market_safe"] 
+__all__ = ["faz17_fetch_market", "faz17_fetch_market_safe"]
