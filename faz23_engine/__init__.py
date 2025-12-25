0ab653271f1bfc96051eaa42113beb2d0b596961
Girayzeynal <girayzeynal@gmail.com>
2025-12-25 23:43:10 +0300

    Create __init__.py

---
+++ faz23_engine/__init__.py
@@ -0,0 +1,11 @@
+# faz23_engine/__init__.py
+"""
+Package initializer for faz23_engine.
+
+Exposes the Faz23Engine class so that callers can do:
+    from faz23_engine import Faz23Engine
+"""
+
+from .faz23_engine import Faz23Engine
+
+__all__ = ["Faz23Engine"]
