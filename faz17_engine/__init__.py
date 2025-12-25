a16a43149d16b68625ff6a27d4b8e7d766337385
Girayzeynal <girayzeynal@gmail.com>
2025-12-25 12:14:13 +0300

    Update __init__.py

--- faz17_engine/__init__.py
+++ faz17_engine/__init__.py
@@ -1,9 +1,14 @@
-from .providers import get_sports, get_odds, send_message, fetch_sports_data
+from .providers import (
+    get_sports_list,
+    get_odds_for_sport,
+    send_message,
+    fetch_sports_data,
+)
 from .faz17_market_fetcher import fetch_market_data
 
 __all__ = [
-    "get_sports",
-    "get_odds",
+    "get_sports_list",
+    "get_odds_for_sport",
     "send_message",
     "fetch_sports_data",
     "fetch_market_data",
