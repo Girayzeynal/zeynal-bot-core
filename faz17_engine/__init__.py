1afa4e859b5e13c93e14870aad57997db442a8a7
Girayzeynal <girayzeynal@gmail.com>
2025-12-26 21:15:52 +0300

    Update __init__.py

--- faz17_engine/__init__.py
+++ faz17_engine/__init__.py
@@ -1,11 +1,5 @@
-# faz17_engine/__init__.py
-"""
-Package initializer for faz17_engine.
-
-Exposes the Faz17Engine class so that callers can do:
-    from faz17_engine import Faz17Engine
-"""
+"""Package initializer for the refactored Faz17Engine."""
 
 from .faz17_engine import Faz17Engine
 
-__all__ = ["Faz17Engine"]
+__all__ = ["Faz17Engine"] 
