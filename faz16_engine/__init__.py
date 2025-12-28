dfc74f5fafe18308a1bc2bae4797ff983758312e
Girayzeynal <girayzeynal@gmail.com>
2025-12-28 07:14:50 +0300

    Update __init__.py

--- faz16_engine/__init__.py
+++ faz16_engine/__init__.py
@@ -5,4 +5,4 @@ try:
 except ModuleNotFoundError:
     _engine_module = importlib.import_module('.engine', package=__name__)
 Faz16Engine = getattr(_engine_module, 'Faz16Engine')
-__all__ = ["faz16_run_simulation", "Faz16Engine"]
+__all__ = ["faz16_run_simulation", "Faz16Engine"] 
