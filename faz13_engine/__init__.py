c0a130ad67760fcd7ffc4c2fc9a6348355d3c17b
Girayzeynal <girayzeynal@gmail.com>
2025-12-29 15:41:45 +0300

    Update __init__.py

--- faz13_engine/__init__.py
+++ faz13_engine/__init__.py
@@ -1,2 +1,27 @@
-from .faz13_engine import Faz13Engine, PrematchRequest, Faz13CoreOutput
+"""
+Initialization for the faz13_engine package.
+
+This module exposes the main engine classes and data structures for
+external imports. It pulls the key classes from the underlying
+`faz13_engine` module so that users can simply do:
+
+    from faz13_engine import Faz13Engine, PrematchRequest, FixtureContext,
+        TeamAverages, Faz13CoreOutput
+"""
+
+from .faz13_engine import (
+    Faz13Engine,
+    PrematchRequest,
+    FixtureContext,
+    TeamAverages,
+    Faz13CoreOutput,
+)
+
+__all__ = [
+    "Faz13Engine",
+    "PrematchRequest",
+    "FixtureContext",
+    "TeamAverages",
+    "Faz13CoreOutput",
+]  
 
